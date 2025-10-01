428a055322bb18e82368b76b8a6a081b278efac1
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-10-01 10:17:23 -0300

    Ping controller

---
+++ src/main/kotlin/com/printscript/tests/PingController.kt
@@ -0,0 +1,11 @@
+package com.printscript.tests
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PingController {
+    @GetMapping("/ping")
+    fun ping(): String = "ok"
+}
+
