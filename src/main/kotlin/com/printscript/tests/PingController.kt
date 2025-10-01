91f277ae5c938deea5b566e193edb450684d9689
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-10-01 10:25:06 -0300

    Build

--- src/main/kotlin/com/printscript/tests/PingController.kt
+++ src/main/kotlin/com/printscript/tests/PingController.kt
@@ -1,11 +1,11 @@
 package com.printscript.tests
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PingController {
     @GetMapping("/ping")
-    fun ping(): String = "ok"
+    fun ping(): ResponseEntity<Void> = ResponseEntity.noContent().build()
 }
-
