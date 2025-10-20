4454b1b04bfded9ab40699f639e5bc46015be758
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-10-20 19:39:36 -0300

    erased * import

--- src/main/kotlin/com/printscript/tests/controller/TestRunController.kt
+++ src/main/kotlin/com/printscript/tests/controller/TestRunController.kt
@@ -4,7 +4,11 @@ import com.printscript.tests.dto.TestRunResponse
 import com.printscript.tests.service.TestRunner
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestRunController(
