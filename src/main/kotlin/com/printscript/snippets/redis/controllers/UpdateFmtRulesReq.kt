6684eeb2b1fad83a5a1ae798845bf40db1a82894
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-11-08 23:32:48 -0300

    Keys

--- src/main/kotlin/com/printscript/snippets/redis/controllers/UpdateFmtRulesReq.kt
+++
@@ -1,5 +0,0 @@
-package com.printscript.snippets.redis.controllers
-
-import com.printscript.snippets.execution.dto.FormatterOptionsDto
-
-data class UpdateFmtRulesReq(val configText: String?, val configFormat: String?, val options: FormatterOptionsDto?)
