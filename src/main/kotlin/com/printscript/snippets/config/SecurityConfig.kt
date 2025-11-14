6b064e7a4f6f89e16b99dbeb5e9176743684aac9
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-11-14 18:57:56 -0300

    permit all route health

--- src/main/kotlin/com/printscript/snippets/config/SecurityConfig.kt
+++ src/main/kotlin/com/printscript/snippets/config/SecurityConfig.kt
@@ -42,6 +42,8 @@ class SecurityConfig(
         http
             .authorizeHttpRequests {
                 it
+                    .requestMatchers("/actuator/**")
+                    .permitAll()
                     // 1. ENDPOINTS DE SNIPPETS (CRUD)
                     // Lectura de snippets y sus tests (read:snippets)
                     .requestMatchers("/internal/**").authenticated()
