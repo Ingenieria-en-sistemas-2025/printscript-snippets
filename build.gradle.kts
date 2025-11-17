cbf4731f156e6b5d76585598718f9e38e1b73406
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-11-17 13:09:18 -0300

    erased actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.newrelic.agent.java:newrelic-api:8.10.0")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
