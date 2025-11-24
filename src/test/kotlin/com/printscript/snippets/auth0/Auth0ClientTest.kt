6cd64a62b9f22d6037747f0413e5fd379a0b6ab9
catalinapereyra <cpereyra@mail.austral.edu.ar>
2025-11-24 10:58:32 -0300

    .

--- src/test/kotlin/com/printscript/snippets/auth0/Auth0ClientTest.kt
+++ src/test/kotlin/com/printscript/snippets/auth0/Auth0ClientTest.kt
@@ -14,7 +14,12 @@ import org.mockito.Mockito.`when`
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.verify
 import org.springframework.core.ParameterizedTypeReference
-import org.springframework.http.*
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 
 class Auth0ClientTest {
